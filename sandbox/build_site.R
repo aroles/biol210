ce74abbefcfdbe721b268475d07c3fcbd01e2b05
aroles <aroles@oberlin.edu>
2024-06-07 11:57:01 -0400

    course agenda 2.0
    Fixed link referencing this new article

--- sandbox/build_site.R
+++ sandbox/build_site.R
@@ -5,6 +5,6 @@ pkgdown::build_site_github_pages()
 
 base_sheets <- file.path(here::here(), 'sheets')
 articles_sheets <- file.path(here::here(), 'docs', 'articles', 'sheets')
-
+̄
 fs::dir_copy(base_sheets, articles_sheets)
 
